8970f5aba87e8205e099dd77e671b6a7d9a822d0
ac-marlon <acmarlon@outlook.com>
2017-10-05 13:17:17 -0500

    Final Final No va mas

--- ScriptFamilia.pl
+++ ScriptFamilia.pl
@@ -21,6 +21,31 @@ padrede('jose','lina').
 padrede('ruben','daniela').
 padrede('ruben','isabel').
 padrede('jesus','jose').
+padrede('daniel', 'francisca').
+padrede('gustavo','francisca').
+padrede('francisca','raul').
+padrede('britany','raul').
+eshombre('juan').
+eshombre('pablo').
+eshombre('carlos').
+eshombre('jesus').
+eshombre('ruben').
+eshombre('gustavo').
+eshombre('luis').
+eshombre('jose').
+eshombre('daniel').
+eshombre('raul').
+esmujer('sofia').
+esmujer('juliana').
+esmujer('daniela').
+esmujer('isabel').
+esmujer('lucia').
+esmujer('maria').
+esmujer('marcela').
+esmujer('pamela').
+esmujer('lina').
+esmujer('francisca').
+esmujer('britany').
 
 tiode(A,B) :-hermanode(A,C), padrede(C,B).
 
@@ -30,22 +55,34 @@ bisabuelode(A,B) :-padrede(A,C), padrede(C,D), padrede(D,B).
 
 bisnietode(A,B) :-bisabuelode(B,A).
 
-hijode(A,B) :-padrede(B,A).
+hijode(A,B) :-padrede(B,A).
 
-abuelode(A,B) :-padrede(A,C), padrede(C,B).
+abuelode(A,B) :-padrede(A,C), padrede(C,B).
 
 nietode(A,B) :-abuelode(B,A).
 
 esposode(A,B) :-padrede(A,C), padrede(B,C).
 
-hermanode(A,B) :-padrede(C,A), padrede(C,B), A \== B.
+hermanode(A,B) :-padrede(C,A), padrede(C,B), A \== B.
 
-primo(A,B) :-hermanode(C,D), hijode(A,C), hijode(B,D).
+primode(A,B) :-hermanode(C,D), hijode(A,C), hijode(B,D).
 
-cuñadode(A,B) :-hermanode(A,C), esposode(C,B).
+cuÃ±adode(A,B) :-hermanode(A,C), esposode(C,B).
 
-familiarde(A,B) :-padrede(A,B).
-familiarde(A,B) :-abuelode(A,B).
-familiarde(A,B) :-hermanode(A,B).
+esfeliz(A) :-esposode(A,B), padrede(B,C), hijode(C,A).
 
+esadoptado(C) :-eshombre(A), eshombre(B), padrede(A,C), padrede(B,C), A\==B.
 
+esadoptado(C) :-esmujer(A), esmujer(B), padrede(A,C), padrede(B,C), A\==B.
+
+familiarde(A,B) :-padrede(A,B).
+familiarde(A,B) :-abuelode(A,B).
+familiarde(A,B) :-hermanode(A,B).
+familiarde(A,B) :-nietode(A,B).
+familiarde(A,B) :-tiode(A,B).
+familiarde(A,B) :-sobrinode(A,B).
+familiarde(A,B) :-bisabuelode(A,B).
+familiarde(A,B) :-bisnietode(A,B).
+familiarde(A,B) :-hijode(A,B).
+familiarde(A,B) :-esposode(A,B).
+familiarde(A,B) :-primode(A,B).
