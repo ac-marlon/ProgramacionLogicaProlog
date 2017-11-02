2f4875fcee806302a708ba387409b9922fb73ae5
ac-marlon <acmarlon@outlook.com>
2017-11-02 12:33:54 -0500

    Final

--- NominaUD.pl
+++ NominaUD.pl
@@ -123,9 +123,8 @@ sumar([],0).
 sumar([ X | Xs ],R) :- sumar(Xs, R1), R is R1+X.
 
 %Calculo de la nomina general en un semestre especifico
-nominaNormal(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*18)*(1+(F/100))))), Lista),                      sumar(Lista,Total).
-
-nominaInter(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*4)*(1+(F/100))))), Lista),                      sumar(Lista,Total).
+nominaNormal(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*18)*(1+(F/100))))), Lista), sumar(Lista,Total).
+nominaInter(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*4)*(1+(F/100))))), Lista), sumar(Lista,Total).
 
 
 
