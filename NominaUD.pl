2760bce506e781bf6151020551eb1ef72956d81b
DanZaky <30821070+DanZaky@users.noreply.github.com>
2017-11-02 10:21:13 -0500

    Terminado

--- NominaUD.pl
+++ NominaUD.pl
@@ -119,9 +119,21 @@ tipoSemestre('Intersemestral',4).
 %Calculo de la nomina individual en un semestre especifico
 nominaEmpleado(A,K,B):-vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F),tipoSemestre(K,S),B is round(((D*E*S)*(1+(F/100)))).
 
-sumar([ X | Y ],R) :- sumar(Y, R1), R is R1+X.
+sumar([],0).
+sumar([ X | Xs ],R) :- sumar(Xs, R1), R is R1+X.
 
 %Calculo de la nomina general en un semestre especifico
 nominaNormal(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*18)*(1+(F/100))))), Lista),                      sumar(Lista,Total).
-nominaInter(Lista) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*4)*(1+(F/100))))), Lista).
+
+nominaInter(Total) :- findall(B, (vinculacion(A,C),horasVinculacion(C,D),categoria(A,P),valorHoraCategoria(C,P,E),liquidacionBonoMensual(C,F), B is round(((D*E*4)*(1+(F/100))))), Lista),                      sumar(Lista,Total).
+
+
+
+
+
+
+
+
+
+
 
